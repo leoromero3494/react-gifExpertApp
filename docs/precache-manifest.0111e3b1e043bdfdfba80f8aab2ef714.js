58dcf7a5928a8c6732acc1c14a57284425eca65d
LeoRomero <leo.romero3494@gmail.com>
2020-11-20 10:43:55 -0700

    docs

---
+++ docs/precache-manifest.0111e3b1e043bdfdfba80f8aab2ef714.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f87023fb35d374c74f6a330838bd1aeb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c4bf050a074143f7b856",
+    "url": "/static/css/main.1d65e7ee.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4bf050a074143f7b856",
+    "url": "/static/js/main.5c4d01e0.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
